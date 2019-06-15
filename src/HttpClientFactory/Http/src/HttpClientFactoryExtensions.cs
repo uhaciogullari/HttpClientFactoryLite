7ef3b60d00d3cc18ff1074e0b282448da5e8fa7e
Ufuk Hacıoğulları <ufukhaciogullari@gmail.com>
2019-06-16 01:03:13 +0200

    Added HttpClientFactoryExtensions

---
+++ src/HttpClientFactory/Http/src/HttpClientFactoryExtensions.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Net.Http;
+using Microsoft.Extensions.Internal;
+
+namespace Microsoft.Extensions.Http
+{
+    public static class HttpClientFactoryExtensions
+    {
+        public static HttpClient CreateClient<T>(this IHttpClientFactory httpClientFactory)
+        {
+            return httpClientFactory.CreateClient(CreateName<T>());
+        }
+
+        public static void Register<T>(this IHttpClientFactory httpClientFactory, Action<IHttpClientFactoryOptionsBuilder> configureOptions)
+        {
+            httpClientFactory.Register(CreateName<T>(), configureOptions);
+        }
+
+        private static string CreateName<T>()
+        {
+            return TypeNameHelper.GetTypeDisplayName(typeof(T), true, true);
+        }
+    }
+}
